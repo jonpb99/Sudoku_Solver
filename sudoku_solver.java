7a5ea0b3531210933958accda12badb5f0b5edda
jonpb99 <95114485+jonpb99@users.noreply.github.com>
2023-03-09 19:58:28 -0500

    Add files via upload

--- sudoku_solver.java
+++ sudoku_solver.java
@@ -11,6 +11,7 @@ public class sudoku_solver {
         {'-','-','-','-','-','-','-','-','-'},{'-','-','-','-','-','-','-','-','-'}};
 
         Scanner input = new Scanner(System.in);
+        //step1- establish board, display board
 
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[i].length; j++) {
@@ -31,6 +32,7 @@ public class sudoku_solver {
 
         //board = enterPuzzle(board, input);
 
+        //step2- user inputs puzzle- 1 shortcut
         //char[][] puzzle = b;
         //boolean sfsg = true;
         //boolean nsfsg = true;
@@ -47,7 +49,7 @@ public class sudoku_solver {
         for(int i = 0; i < 10; i++){
             candidatelist.add(candidate0);
         }
-        for(int i = 0; i < 10; i++){
+        for(int i = 1; i < 10; i++){
             candidateall.add(i);
         }
 
@@ -60,11 +62,9 @@ public class sudoku_solver {
         System.out.println(candidateedit);
         candidatelist.set(11, candidateedit);
         System.out.println(candidatelist);*/
-
-
         
         
-        for(int i = 0; i < 9; i++){
+        for(int i = 0; i < 3; i++){ //shortcut for debug- 'i' will be 9 at finish
             //int tot = i+1;
             System.out.println("Enter line " + (i+1) +" of puzzle (horizontal)");
             candidate0 = new ArrayList<Integer>();
@@ -117,7 +117,7 @@ public class sudoku_solver {
             System.out.println();
             e = input.nextLine().charAt(0);
             if(e == 'Y' || e == 'y'){
-                if(i < 8){
+                if(i < 2){// shortcut for debug- should be (i < 8)
                     continue;
                 }
             }
@@ -125,7 +125,6 @@ public class sudoku_solver {
                 i--;
                 continue;
             }
-
             System.out.println("Is this puzzle correct? (Y/N):");
             while(notok){
                 for (int ii = 0; ii < board.length; ii++){
@@ -145,7 +144,7 @@ public class sudoku_solver {
                     }
                 }
                 notok = false;
-                System.out.println();
+                //System.out.println();
                 e = input.nextLine().charAt(0);
                 if(e == 'Y' || e == 'y'){
 
@@ -161,15 +160,61 @@ public class sudoku_solver {
         //System.out.println(candidatelist);
         //end enterpuzzle
 
+        //step3- narrow down possibilities in boxes
         for(int i = 11; i < 20; i++){
             System.out.print(candidatelist.get(i));
         }
+        System.out.println();
+
+
 
         narrow_down(candidatelist, candidateedit, board);
+        //int[] box1 = {11,12,13,21,22,23,31,32,33};
+        //int[] box2 = {14,15,16,24,25,26,34,35,36};
+        //int[] box3 = {17,18,19,27,28,29,37,38,39};
+        //int[] box4 = {41,42,43,51,52,53,61,62,63};
+        //int[] box5 = {44,45,46,54,55,56,64,65,66};
+        //int[] box6 = {47,48,49,57,58,59,67,68,69};
+        //int[] box7 = {71,72,73,81,82,83,91,92,93};
+        //int[] box8 = {74,75,76,84,85,86,94,95,96};
+        //int[] box9 = {77,78,79,87,88,89,97,98,99};
+        //box_nd(box1, num, list, e, b);
+        //box_nd(box2, num, list, e, b);
+
+        //ArrayList<Integer> nu = new ArrayList<Integer>();
+        //box_nd(box3, nu, candidatelist, candidateedit, board);
+        /*int n;
+        for(int i : box3){
+            int v = i % 10;
+            int h = (i - v) / 10;
+            if(board[h-1][v-1] != '-'){
+                n = (int) board[h-1][v-1] - '0';
+                nu.add(n);
+            }
+        }
+        System.out.println(nu);
+        for(int i : box3){
+            int v = i % 10;
+            int h = (i - v) / 10;
+            if(board[h-1][v-1] == '-'){
+                candidateedit = candidatelist.get(i);
+                for(int j = 0; j < nu.size(); j++){
+                    for(int k = 0; k < candidateedit.size(); k++){
+                        if(nu.get(j) == candidateedit.get(k)){
+                            candidateedit.remove(k);
+                        }
+                    }                    
+                }
+                candidatelist.set(i, candidateedit);
+            }
+        }*/
+
+
 
         for(int i = 11; i < 20; i++){
             System.out.print(candidatelist.get(i));
         }
+        System.out.println();
 
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[i].length; j++) {
@@ -223,8 +268,11 @@ public class sudoku_solver {
         int[] box8 = {74,75,76,84,85,86,94,95,96};
         int[] box9 = {77,78,79,87,88,89,97,98,99};
         box_nd(box1, num, list, e, b);
+        num.clear();
         box_nd(box2, num, list, e, b);
-        //box_nd(box3, num, list, e, b);
+        num.clear();
+        box_nd(box3, num, list, e, b);
+        //num.clear();
         //box_nd(box4, num, list, e, b);
         //box_nd(box5, num, list, e, b);
         //box_nd(box6, num, list, e, b);
@@ -264,15 +312,16 @@ public class sudoku_solver {
         for(int i : bx){
             int v = i % 10;
             int h = (i - v) / 10;
-            if(b[h][v] != '-'){
-                n = (int) b[h][v];
+            if(b[h-1][v-1] != '-'){
+                n = (int) b[h-1][v-1] - '0';
                 num.add(n);
             }
         }
+        System.out.println(num);
         for(int i : bx){
             int v = i % 10;
             int h = (i - v) / 10;
-            if(b[h][v] == '-'){
+            if(b[h-1][v-1] == '-'){
                 e = l.get(i);
                 for(int j = 0; j < num.size(); j++){
                     for(int k = 0; k < e.size(); k++){
